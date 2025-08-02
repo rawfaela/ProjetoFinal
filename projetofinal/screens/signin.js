bcf75a614e348af35826077166d0616a94ad4ee6
rafa <rafaelaaghellere@gmail.com>
2025-08-02 10:17:42 -0300

    Update signin.js

--- projetofinal/screens/signin.js
+++ projetofinal/screens/signin.js
@@ -3,19 +3,21 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SignIn(){
     return(        
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>NOME</Text>
-            <Image source={require('../assets/logofake.png')}
-            style={{ width: 150, height: 150, marginVertical: 20}}/>
-            <Text style={styles.title2}>CADASTRO</Text>
-            <View style={{rowGap: 20, width: '100%', alignItems:'center'}}>
-                <TextInput placeholder='Email' style={styles.input}></TextInput>
-                <TextInput placeholder='Senha' style={styles.input}></TextInput>
-            </View>
-            <Text style={styles.error}>{/* {error} */}erros</Text>
-            <View style={{rowGap: 20}}>
-                <TouchableOpacity style={styles.button}><Text style={styles.buttontext}>Criar conta</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.button}><Text  style={styles.buttontext}>Voltar ao login</Text></TouchableOpacity>
+        <SafeAreaView style={{flex: 1, backgroundColor: '#9ebc8a'}}>
+            <View style={styles.container}>
+                <Text style={styles.title}>NOME</Text>
+                <Image source={require('../assets/logofake.png')}
+                style={{ width: 150, height: 150, marginVertical: 20}}/>
+                <Text style={styles.title2}>CADASTRO</Text>
+                <View style={{rowGap: 20, width: '100%', alignItems:'center'}}>
+                    <TextInput placeholder='Email' style={styles.input}></TextInput>
+                    <TextInput placeholder='Senha' style={styles.input}></TextInput>
+                </View>
+                <Text style={styles.error}>{/* {error} */}erros</Text>
+                <View style={{rowGap: 20}}>
+                    <TouchableOpacity style={styles.button}><Text style={styles.buttontext}>Criar conta</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.button}><Text  style={styles.buttontext}>Voltar ao login</Text></TouchableOpacity>
+                </View>
             </View>
         </SafeAreaView>
     )
@@ -32,7 +34,7 @@ const styles = StyleSheet.create({
         fontSize: 35,
         marginTop: 35,
         fontWeight: 800,
-        color: '#5f6f52',
+        color: '#3b3b1a',
     },
     title2:{
         fontSize: 25,
@@ -59,7 +61,7 @@ const styles = StyleSheet.create({
         marginVertical: 15,
     },
     button:{
-        backgroundColor:'#3b3b1a',
+        backgroundColor:'#5f6f52',
         alignItems: 'center',
         borderRadius: 17,
         padding: 9,
