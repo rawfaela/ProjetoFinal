eeb50166041c5084a7c96b551045f7fa9a104c11
rafa <rafaelaaghellere@gmail.com>
2025-08-18 10:13:59 -0300

    Update App.js

--- projetofinal/App.js
+++ projetofinal/App.js
@@ -103,6 +103,7 @@ import AddProducts from './screens/addProducts';
 import BottomTabs from './components/bottomtabs';
 import Stock from './screens/stock';
 import Orders from './screens/orders';
+import MoreInfo from './screens/moreInfo';
 
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
