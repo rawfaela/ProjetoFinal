5409a80caba39dae4f423840a4efd765959a7a3f
rafa <rafaelaaghellere@gmail.com>
2025-08-18 11:26:59 -0300

    coisas

--- projetofinal/utils/controller.js
+++ projetofinal/utils/controller.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC3_mH3DiRXFb3640qHSFy37ZBhcL_yXsc",
@@ -17,4 +17,3 @@ export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
 export const db = getFirestore(app);
-export const storage = getStorage(app);
